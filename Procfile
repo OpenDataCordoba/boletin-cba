de403c99c01654d24d538c850c0f91579698c48f
Gaston Avila <avila.gas@gmail.com>
2017-05-23 13:28:30 -0300

    pruebas con scrapinghub

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn app:app -R --error-logfile=- --access-logfile=- --reload
 scrape: scrapy crawl boe
+dbupgrade: FLASK_APP=app.py flask db upgrade
--- boescraper/settings.py
+++ boescraper/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'boescraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'boescraper (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
